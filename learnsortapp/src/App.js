9976d24e5dc4b966bea534772cbce2091dca0dbf
stella lee <jlee3465@uwo.ca>
2022-01-28 16:05:10 -0500

    fixes

--- learnsortapp/src/App.js
+++ learnsortapp/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from "./navbar/Navbar";
@@ -15,22 +14,6 @@ function App() {
             </Routes>
         </Router>
     </>
-=======
-import Navbar from "./Navbar";
-import Dashboard from "./pages/Dashboard";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Levels from "./pages/Levels";
-
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-         <Route path='/' exact element={<Dashboard/>} />
-         <Route path='/levels' element={<Levels/>} />
-      </Routes>
-    </Router>
->>>>>>> b4455d65cc7be5ea848710f189cbbda0dcb6a623
   );
 }
 
--- learnsortapp/src/pages/Dashboard.js
+++ learnsortapp/src/pages/Dashboard.js
@@ -1,12 +1,6 @@
 import React, {useState} from 'react';
 
 function Dashboard() {
-<<<<<<< HEAD:learnsortapp/src/Dashboard.js
-=======
-    // const [click] = useState(false);
-    
->>>>>>> b4455d65cc7be5ea848710f189cbbda0dcb6a623:learnsortapp/src/pages/Dashboard.js
-
     // put dashboard contents under return
     return (
         <div>
