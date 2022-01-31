03e6b46026592d0274213c62f27b78080179b178
mveysey <mveysey@uwo.ca>
2022-01-31 11:08:43 -0500

    Finished Levels Page

--- learnsortapp/src/pages/Levels.js
+++ learnsortapp/src/pages/Levels.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import './Levels.css'
 
 function Levels() {
-    // put dashboard contents under return
     return (
         <>
         <div class = "Rectangle15">
@@ -14,6 +13,36 @@ function Levels() {
             <Link to='/Level1'>
                 <div class = "Rectangle2">
                     <h3 class= "Level1Text">Level 1</h3>
+                </div>
+            </Link>
+
+            <Link to = ''>
+                <div class = 'RectangleLvl2'>
+                    <h3 class = "Level2Text">Level 2 (Coming Soon)</h3>
+                </div>
+            </Link>
+
+            <Link to = ''>
+                <div class = 'RectangleLvl3'>
+                    <h3 class = "Level3Text">Level 3 (Coming Soon)</h3>
+                </div>
+            </Link>
+
+            <Link to = ''>
+                <div class = 'RectangleLvl4'>
+                    <h3 class = "Level4Text">Level 4 (Coming Soon)</h3>
+                </div>
+            </Link>
+
+            <Link to = ''>
+                <div class = 'RectangleLvl5'>
+                    <h3 class = "Level5Text">Level 5 (Coming Soon)</h3>
+                </div>
+            </Link>
+
+            <Link to = ''>
+                <div class = 'RectangleLvlCustom'>
+                    <h3 class = "LevelCustomText">Custom Level (Coming Soon)</h3>
                 </div>
             </Link>
             
