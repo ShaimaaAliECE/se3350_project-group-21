f80aa14dfef2a148348b93f01a5b95586f516dc8
stella lee <jlee3465@uwo.ca>
2022-01-30 20:22:15 -0500

    finished dashboard page

--- learnsortapp/src/pages/Dashboard.js
+++ learnsortapp/src/pages/Dashboard.js
@@ -1,13 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Dashboard.css'
 
 function Dashboard() {
     // put dashboard contents under return
     return (
-        <div>
-            <h1>
-                <Link to='/Levels'>Go to Merge Sort Levels</Link>  
-            </h1>
+        <div className="dashboardcontents">
+            <h1 className="dashboardheader">What would you like to learn?</h1>
+            <div className="square-container">
+                {/* copy this div to make another algorithm box */}
+                <div className="square">
+                    <p className="algorithm-name">Merge Sort</p>
+                    <div className="bottom-square">
+                        <p className="levelnumbers">6 Levels</p>
+                    </div>
+                    <div className="circle"></div>
+                    
+                     <Link to='/Levels' className="levelslink">
+                        <div className="circle2">
+                            <p className="go">GO!</p>
+                        </div>
+                    </Link>  
+                </div>
+                {/* end of one algorithm box */}
+                
+                <div className="square">
+                    <p className="algorithm-name">Coming Soon!</p>
+                    <div className="bottom-square">
+                        <p className="levelnumbers">6 Levels</p>
+                    </div>
+                    <div className="circle"></div>
+                    
+                     <Link to='' className="levelslink">
+                        <div className="circle2">
+                            <p className="go">GO!</p>
+                        </div>
+                    </Link>  
+                </div>
+
+                <div className="square">
+                    <p className="algorithm-name">Coming Soon!</p>
+                    <div className="bottom-square">
+                        <p className="levelnumbers">6 Levels</p>
+                    </div>
+                    <div className="circle"></div>
+                    
+                     <Link to='' className="levelslink">
+                        <div className="circle2">
+                            <p className="go">GO!</p>
+                        </div>
+                    </Link>  
+                </div>
+            </div>
         </div>
     );
 }
