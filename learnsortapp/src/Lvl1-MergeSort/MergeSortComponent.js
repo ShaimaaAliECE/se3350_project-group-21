1c266daad1f7de23fdc20ec1e116f7f1ee8bf0c6
Dana <dalkwifi@uwo.ca>
2022-02-02 15:58:51 -0500

    styling to level 1 page

--- learnsortapp/src/Lvl1-MergeSort/MergeSortComponent.js
+++ learnsortapp/src/Lvl1-MergeSort/MergeSortComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { MergeSort } from './MergeSort';
 import { Partition } from './Partition';
-import { Button } from './Button';
 import './Level1.css';
 
 export default class MergeSortComponent extends Component {
@@ -20,10 +19,17 @@ export default class MergeSortComponent extends Component {
     
   }
 
+  randomNum(){
+    let r = Array.from({length: 10}, () => Math.floor(Math.random() * 21));
+    return r;
+
+  }
+
   nextStep(){
     let partition = new Partition(0, this.unsorted);
     this.mergeSort1.mergeSort(partition);
-    this.setState({partitions: this.mergeSort1.partitions});  
+    this.setState({partitions: this.mergeSort1.partitions});
+
   }
 
   // will be called after the component is rendered
@@ -41,58 +47,64 @@ export default class MergeSortComponent extends Component {
     // get each partition and map each node 
     let fragments = this.state.partitions.map((node, i1) => {
       // for each fragment row
-        return <div key={i1} className="fragment-row" >
-                {
-                  node.fragments.map((numbers, i2) =>
-                  <span>
-                    <span className="group" key={i2}>
-                    {
-                      numbers.map((number, index) => {
-                        {/* will display the first half of each row (before "split from" or "merged from") */}
-                        return <span key={index} className="number"> {number} </span>
-                      })
-                    }
-                    </span>
-                  </span>
-                  )
-                }
-                
-                {/* will either be "split from" or "merged from" */}
-                <span>{node.descr}</span>
+        return <div key={i1} className="fragment" >
+        {
+        
+          node.fragments.map((numbers, i2) =>
+          <span>
+            <span className="group" key={i2}>
+            {
+              numbers.map((number, index) => {
+                {/* will display the first half of each row (before "split from" or "merged from") */}
+                return <span key={index} className="number"> {number} </span>
+              })
+            }
+            </span>
+          </span>
+          )
+        }
+        
+        {/* will either be "split from" or "merged from" */}
+        
 
                 {/* will display the first half of the right side (after "split from" or "merged from") */}
-                <span className={node.show}>
-                  {(node.part1 || []).map((n, index) => {
-                    return <span key={index} className="number"> {n} </span>
-                  })}
-                </span>
+               
               
                 {/* will display the second half of the right side (after "split from" or "merged from") */}
-                <span className="group">
-                  {(node.part2 || []).map((n, index) => {
-                    return <span key={index} className="number"> {n} </span>
-                  })}
-                </span>
+                
+                
               </div>
+              
     });
+    
       return (
         <>
             <div class="contents">
                <div class = "Rectangle15">
                   <h3 class = "MergeSortText">Merge Sort</h3>
                </div>
-            </div>
+               <button className='btn' onClick={this.unsorted.join(' ')}>Generate Numbers</button>
+            </div>            
             <div className="fragment-row">
                {/* // will show the initial values (created by random number generator) */}
-               <strong>Sample Numbers: { this.unsorted.join(' ') }</strong>
-               <br/>
-               <button onClick={this.nextStep}>Next</button>
+               <div className="values"> 
+               Your Values <br/>
+               </div>
+               <div className='randomNum'>
+               { this.unsorted.join(', ') }
                
+               
+               <br/>
+               </div>
+               
+               <button className="forward" onClick={this.nextStep}>Next</button>
+               {/* this is where the rows of the output are printed
+               each fragment is a row of the output, e.g. fragments[0] is row 1... */}
+               
+               {fragments}
                
             </div>
-            {/* this is where the rows of the output are printed
-            each fragment is a row of the output, e.g. fragments[0] is row 1... */}
-            {fragments}
+            
         </>
         );
   }
