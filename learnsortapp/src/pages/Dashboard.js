ae1ce3b5213e3ec13a317940e49624f3d22c1391
JonathanRichards <jrich87@uwo.ca>
2022-02-03 10:25:25 -0500

    Editing styling to searched content is centered

--- learnsortapp/src/pages/Dashboard.js
+++ learnsortapp/src/pages/Dashboard.js
@@ -11,8 +11,9 @@ function Dashboard() {
             <div className="dashboardcontents">
                 <h1 className="dashboardheader">What would you like to learn?</h1>
 
-                <div className = "Search">
+                <div className = "searchBar">
                     <input type="text" placeholder="Search Algorithm" onChange={event => {setSearchTerm(event.target.value)}}/>
+                    <div className="searchContent">
                     {JSONDATA.filter((val)=>{
                         if (searchTerm == "") {
                             return val
@@ -40,7 +41,7 @@ function Dashboard() {
                 </div>
 
                 
-                
+                </div>
             </div>
         </>
     )
