51d096a00ee943fe3ed5badcdd4970e190e5e52e
Reuben Anchan <reuben.anchan@gmail.com>
2022-02-03 03:03:06 -0500

    Created Search Bar

--- learnsortapp/src/pages/Dashboard.js
+++ learnsortapp/src/pages/Dashboard.js
@@ -1,57 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Dashboard.css'
+import JSONDATA from "./AlgorithmSelect.json"
+import {useState} from 'react'
 
 function Dashboard() {
+    const [searchTerm, setSearchTerm] = useState('')
     return (
         <>
             <div className="dashboardcontents">
                 <h1 className="dashboardheader">What would you like to learn?</h1>
-                <div className="square-container">
-                    {/* copy this div to make another algorithm box */}
-                    <div className="square">
-                        <p className="algorithm-name">Merge Sort</p>
-                        <div className="bottom-square">
-                            <p className="levelnumbers">6 Levels</p>
-                        </div>
-                        <div className="circle"></div>
-                        
-                        <Link to='/Levels' className="levelslink">
-                            <div className="circle2">
-                                <p className="go">GO!</p>
-                            </div>
-                        </Link>  
-                    </div>
-                    {/* end of one algorithm box */}
-                    
-                    <div className="square">
-                        <p className="algorithm-name">Coming Soon!</p>
-                        <div className="bottom-square">
-                            <p className="levelnumbers">6 Levels</p>
-                        </div>
-                        <div className="circle"></div>
-                        
-                        <Link to='' className="levelslink">
-                            <div className="circle2">
-                                <p className="go">GO!</p>
-                            </div>
-                        </Link>  
-                    </div>
 
-                    <div className="square">
-                        <p className="algorithm-name">Coming Soon!</p>
-                        <div className="bottom-square">
-                            <p className="levelnumbers">6 Levels</p>
-                        </div>
-                        <div className="circle"></div>
-                        
-                        <Link to='' className="levelslink">
-                            <div className="circle2">
-                                <p className="go">GO!</p>
-                            </div>
-                        </Link>  
-                    </div>
+                <div className = "Search">
+                    <input type="text" placeholder="Search Algorithm" onChange={event => {setSearchTerm(event.target.value)}}/>
+                    {JSONDATA.filter((val)=>{
+                        if (searchTerm == "") {
+                            return val
+                        } else if (val.algorithm.toLowerCase().includes(searchTerm.toLowerCase())) {
+                            return val
+                        }
+                    }).map((val, key)=> {
+                        return <div className="algorithm format" key={key}>
+                                    <div className="square">
+                                        <p className="algorithm-name">{val.algorithm}</p>
+                                        <div className="bottom-square">
+                                        <p className="levelnumbers">6 Levels</p>
+                                        </div>
+                                        <div className="circle"></div>
+                                        
+                                        <Link to={val.link} className="levelslink">
+                                            <div className="circle2">
+                                                <p className="go">GO!</p>
+                                            </div>
+                                        </Link>  
+                                    </div>
+                                    
+                                </div>
+                    })}
                 </div>
+
+                
+                
             </div>
         </>
     )
