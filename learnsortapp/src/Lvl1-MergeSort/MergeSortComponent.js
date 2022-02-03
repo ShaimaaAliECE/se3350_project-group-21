98e918dc1abb102865abe5bebd3c37662fcf8f9d
mveysey <mveysey@uwo.ca>
2022-02-03 13:49:05 -0500

    Put Level 1 animation into proper position

--- learnsortapp/src/Lvl1-MergeSort/MergeSortComponent.js
+++ learnsortapp/src/Lvl1-MergeSort/MergeSortComponent.js
@@ -26,6 +26,7 @@ export default class MergeSortComponent extends Component {
   }
 
 
+  // start running the algorithm
   nextStep(){
     let partition = new Partition(0, this.unsorted);
     this.mergeSort1.mergeSort(partition);
@@ -38,6 +39,7 @@ export default class MergeSortComponent extends Component {
 
   }
 
+  // display the next step in the algorithm with text
   IncrementItem = () => {
     this.setState({ arrayIndex: this.state.arrayIndex + 1 });
     const i = ["Step 1(a): Find the middle index of the array, and divide the array into two parts from the middle. This is the left side:","Step 1(b): This is the right side:", "Step 2(a): Now starting from the left half of the array, we are going to continue to divide each sub-array in half (as evenly as possible). This is the first half of the left sub-array:", "Step 2(b): This is the second half of the left sub-array:", "Step 3(a): Now, we will continue to break down the left sub-arrays until each element is separated. During this process we will also begin comparing elements to order them in ascending order. ", "Step 3(b): Continue breaking down the sub-arrays into individual elements: ", "Step 4: Now we have all of our elements separated, we can start to compare the elements of the left sub-array and sort them in ascending order.", "Step 5: Merge all of the left sub-array elements, now sorted in ascending order. ", "Step 6(a): We will now repeat the process to the right sub-array. Split the right sub-array in half (as evenly as possible). This is the first half: ", "Step 6(b): This is the second half: ", "Step 7(a): Continue to break down the right sub-arrays until they are all just one element. We will also begin comparing elements to ensure they are in ascending order. ", "Step 7(b): Continue splitting the right sub-arrays that are still not single elements: ", "Step 8(a): Now we can begin comparing all of the right sub-array elements and sort them in ascending order", "Step 8(b): Merge the right sub-arrays in ascending order.", "Step 15: Merge the now sorted left subarray, and right subarray to get the final sorted list."];
@@ -47,7 +49,7 @@ export default class MergeSortComponent extends Component {
     instructionBox.innerHTML = instructionID;
 
     document.getElementById(elementID).style.display = 'block';
-    document.getElementById(elementID).style.textAlign = 'center';
+    // document.getElementById(elementID).style.textAlign = 'center';
     document.getElementById(elementID).style.animation = 'pulse 2s';
     document.getElementById(elementID).style.fontSize = '12px';  
 
@@ -88,19 +90,18 @@ export default class MergeSortComponent extends Component {
     stepsArray[1] = fragments[9];
     stepsArray[2] = fragments[2];
     stepsArray[3] = fragments[4];
-    stepsArray[4] = fragments[3];
-    stepsArray[5] = fragments[5];
+    stepsArray[4] = fragments[5];
+    stepsArray[5] = fragments[3];
     stepsArray[6] = fragments[7];
     stepsArray[7] = fragments[8];
     stepsArray[8] = fragments[10];
     stepsArray[9] = fragments[12];
-    stepsArray[10] = fragments[11];
-    stepsArray[11] = fragments[13];
+    stepsArray[10] = fragments[13];
+    stepsArray[11] = fragments[11];
     stepsArray[12] = fragments[15];
     stepsArray[13] = fragments[16];
     stepsArray[14] = fragments[17];
-
-
+    
       return (
         <>
             <div class="contents">
@@ -153,7 +154,6 @@ export default class MergeSortComponent extends Component {
                   <div className="sorted-array">{sortedArray}</div>
                   <div className='sorted-text'>Sorted List: </div>
                   
-                  {/* <div onClick={this.nextStep} className="continue-button">Continue</div> */}
                   <div  className="back-button">Go Back</div>
                 </div>
                 </div>
