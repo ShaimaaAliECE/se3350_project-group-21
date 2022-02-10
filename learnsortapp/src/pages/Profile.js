7647b5e51b9daa1f14d3d4eb97a59765aea0d1c3
JonathanRichards <jrich87@uwo.ca>
2022-02-10 15:50:10 -0500

    Filling in a basic outline for the profile page based on the instructions outline

---
+++ learnsortapp/src/pages/Profile.js
@@ -0,0 +1,24 @@
+import React, { Profiler } from 'react';
+import './Instructions.css';
+
+function Profile() {
+    return (
+        <>
+            <div class="instructions-contents">
+                <h2 class = "instructions-title-background"></h2>
+                <h3 class = "instructions-title">Instructions</h3>
+
+                <div class = "instructions-Outline">
+                <div class = "instructions-BackgroundRectangle">
+                    <h2>Profile</h2>
+                    <p>(Text here)</p>
+                </div>
+                </div>
+            </div>
+        </>
+        
+    );
+}
+
+
+export default Profile;
