13d987472bc982eebe1cb8501bd405fdd70679b6
mveysey <mveysey@uwo.ca>
2022-02-10 17:01:07 -0500

    Edits to level 2

--- learnsortapp/src/Lvl2-MergeSort/MergeSortComponent1.js
+++ learnsortapp/src/Lvl2-MergeSort/MergeSortComponent1.js
@@ -17,10 +17,12 @@ export default class MergeSortComponent1 extends Component {
     };
     this.mergeSort1 = new MergeSort1();
   }
+
   closeBox(){
     var popup = document.getElementById("myPopup");
     popup.style.visibility = "hidden"; 
   }
+
   // random number generator
   randomNum = () => {
     let r = Array.from({length: 10}, () => Math.floor(Math.random() * 20)+1);
@@ -37,16 +39,23 @@ export default class MergeSortComponent1 extends Component {
     document.getElementById('test-button1').style.display =  'none'; 
     document.getElementById('instruction-box1').style.display = 'block';
     document.getElementById('next-button1').style.display = 'block'; 
-    document.getElementById('back-button1').style.display = 'block'; 
+    document.getElementById('userInput0').style.display = 'block';
   }
 
   // display the next step in the algorithm with text
   IncrementItem = () => {
     this.setState({ arrayIndex: this.state.arrayIndex + 1 });
     const i = ["Step 1(b): This is the right side:", "Step 2(a): Now starting from the left half of the array, we are going to continue to divide each sub-array in half (as evenly as possible). This is the first half of the left sub-array:", "Step 2(b): This is the second half of the left sub-array:", "Step 3(a): Now, we will continue to break down the left sub-arrays until each element is separated. During this process we will also begin comparing elements to order them in ascending order. ", "Step 3(b): Continue breaking down the sub-arrays into individual elements: ", "Step 4: Now we have all of our elements separated, we can start to compare the elements of the left sub-array and sort them in ascending order.", "Step 5: Merge all of the left sub-array elements, now sorted in ascending order. ", "Step 6(a): We will now repeat the process to the right sub-array. Split the right sub-array in half (as evenly as possible). This is the first half: ", "Step 6(b): This is the second half: ", "Step 7(a): Continue to break down the right sub-arrays until they are all just one element. We will also begin comparing elements to ensure they are in ascending order. ", "Step 7(b): Continue splitting the right sub-arrays that are still not single elements: ", "Step 8(a): Now we can begin comparing all of the right sub-array elements and sort them in ascending order", "Step 8(b): Merge the right sub-arrays in ascending order.", "Step 15: Merge the now sorted left subarray, and right subarray to get the final sorted list."];
+    let elementID = "test" + this.state.arrayIndex;
+    let userInputID = "userInput" + this.state.arrayIndex;
     let instructionBox = document.getElementById("instruction-box1");
     let instructionID = i[this.state.arrayIndex];
     instructionBox.innerHTML = instructionID;
+
+    document.getElementById(elementID).style.display = 'block';
+    document.getElementById(elementID).style.animation = 'pulse 1s';
+    document.getElementById(elementID).style.fontSize = '20px';  
+    document.getElementById(userInputID).style.display = 'block';
   }
 
   handleSubmit = (event) => {
@@ -64,16 +73,13 @@ export default class MergeSortComponent1 extends Component {
     var popup = document.getElementById("myPopup");
     var popupText = document.getElementById("poptext");
     popup.style.visibility = "visible"; 
-    
 
     if(event.target.userInput.value == answer.toString()){
-        //alert("correct");
         popupText.innerHTML = "Correct"; 
     }
     else
       popupText.innerHTML = "Incorrect";      
   }
-
 
   render() {
     // get each partition and map each node 
@@ -118,7 +124,6 @@ export default class MergeSortComponent1 extends Component {
     stepsArray[13] = fragments[16];
     stepsArray[14] = fragments[17];
 
-    
       return (
         <>
             <div class="contents">
@@ -137,16 +142,22 @@ export default class MergeSortComponent1 extends Component {
                     </div>
                     <div className='randomNum1'> { this.unsorted.join(', ') } </div>
 
+
+                  {
+                    this.state.show? <div><h1>
+                      {stepsArray}
+                    </h1></div> : null
+                  }
                   <div onClick={this.runAlgorithm} id="test-button1" className="continue-button1">Start!</div>
                   <div onClick={this.IncrementItem} id="next-button1" class="next-button1">Next Step</div>
-                  <div onClick={this.IncrementItem} id="back-button1" class="back-button1">Back</div>
                   <div class="popup" >
                   <span class="popuptext" id="myPopup"><br/><br/><br/><div id="poptext">Correct</div><button class="popnext" onClick={this.closeBox}>Close</button></span>
                   </div>
 
                   <div id="instruction-box1" class="instructions1">Step 1(a): Find the middle index of the array, and divide the array into two parts from the middle. This is the left side:</div>
 
-                  <div>
+
+                  <div className="userInput0" id="userInput0">
                   <form onSubmit={this.handleSubmit}>
                     <label>
                       Answer:
@@ -158,7 +169,191 @@ export default class MergeSortComponent1 extends Component {
                         <button type="submit">Check your answer</button>
                     </form>
                   </div>
-                  
+
+                  <div className="userInput1" id="userInput1">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput2" id="userInput2">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput3" id="userInput3">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput4" id="userInput4">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput5" id="userInput5">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput6" id="userInput6">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput7" id="userInput7">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput8" id="userInput8">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput9" id="userInput9">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput10" id="userInput10">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput11" id="userInput11">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput12" id="userInput12">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput13" id="userInput13">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+                  <div className="userInput14" id="userInput14">
+                  <form onSubmit={this.handleSubmit}>
+                    <label>
+                      Answer:
+                      <input 
+                        type="text" 
+                        name="userInput"
+                      />
+                        </label>
+                        <button type="submit">Check your answer</button>
+                    </form>
+                  </div>
+
+
+
                   <div className="test0" id="test0">{stepsArray[0]}</div>
                   <div className="test1" id="test1">{stepsArray[1]}</div>
                   <div className="test2" id="test2">{stepsArray[2]}</div>
@@ -176,7 +371,7 @@ export default class MergeSortComponent1 extends Component {
                   <div className="test14" id="test14">{stepsArray[14]}</div>
                   <br/><br/>
                   
-                  
+                  <div  className="back-button1">Go Back</div>
                 </div>
                 </div>
         </>
