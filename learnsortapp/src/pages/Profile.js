3e59643d870f82d7ed53acb88f442a1f79fd2fc2
JonathanRichards <jrich87@uwo.ca>
2022-02-10 15:52:07 -0500

    Completed basic files to make a profile page

--- learnsortapp/src/pages/Profile.js
+++ learnsortapp/src/pages/Profile.js
@@ -1,15 +1,15 @@
 import React, { Profiler } from 'react';
-import './Instructions.css';
+import './Profile.css';
 
 function Profile() {
     return (
         <>
-            <div class="instructions-contents">
-                <h2 class = "instructions-title-background"></h2>
-                <h3 class = "instructions-title">Instructions</h3>
+            <div class="profile-contents">
+                <h2 class = "profile-title-background"></h2>
+                <h3 class = "profile-title">Profile</h3>
 
-                <div class = "instructions-Outline">
-                <div class = "instructions-BackgroundRectangle">
+                <div class = "profile-Outline">
+                <div class = "profile-BackgroundRectangle">
                     <h2>Profile</h2>
                     <p>(Text here)</p>
                 </div>
