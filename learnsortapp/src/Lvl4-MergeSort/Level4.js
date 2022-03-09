09e832b03135717b3a75aeacf713db744bcd412d
harshitbhatia180 <hbhati3@uwo.ca>
2022-03-09 16:30:33 -0500

    level 4

--- learnsortapp/src/Lvl4-MergeSort/Level4.js
+++ learnsortapp/src/Lvl4-MergeSort/Level4.js
@@ -13,7 +13,7 @@ function Level4() {
             {/* info about the specific algorithm */}
             <div class = "Outline3">
                 <div class = "BackgroundRectangle3">
-                <h2>Level 5</h2>
+                <h2>Level 4</h2>
                 <p>Now that you have completed Level 3 of merge sort, you can proceed to Level 4!</p>
                 <p>As done in previous levels , you'll be given an unsorted list but no description of the steps in the merge sort algorithm.</p>
                 <p>You'll be asked to input the correct numbers, which you can check with the "check my answer" button.</p>
@@ -21,9 +21,15 @@ function Level4() {
                 <p>Once you are ready to begin, you can press the Start button!</p>
                 </div>
 
+                <Link to='/MergeSortComponent3' class = "continue-link1">
+                  <div class = "ContinueBox2">
+                     <h3 class = "ContinueText2">Start!</h3>
+                  </div>
+               </Link>
+
                <Link to='/Levels' class = "continue-link1">
                   <div class = "BackBox3">
-                     <h3 class = "BackText3">Back to levels page</h3>
+                     <h3 class = "BackText3">Levels page</h3>
                   </div>
                </Link>
             </div>
