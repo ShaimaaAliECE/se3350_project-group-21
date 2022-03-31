34fb435c16435ea50608bd0e6a733fc9a1347bb1
JonathanRichards <jrich87@uwo.ca>
2022-03-31 14:26:05 -0400

    Just updating the instructions page

--- learnsortapp/src/pages/Instructions.js
+++ learnsortapp/src/pages/Instructions.js
@@ -15,17 +15,29 @@ function Instructions() {
 
                     <h2>What are its Functions?</h2>
 
-                    <h4>Course Page</h4>
+                    <h4>Get Started!</h4>
                     <p>When users first login, they’ll be presented with the main dashboard which contains all possible lessons (sorting algorithms) to learn. From there, they’ll be able to choose a specific algorithm and begin learning. Currently learning is only available for merge sort, however we intend to add additional learning modules.</p>
-                
+
+                    <h4>Profile Page</h4>
+                    <p>The profile page displays all of the Top Times reached by the user at each level. The top times are stored locally for the user. As well, user analytics are provided as a graphical representation.</p>
+
                     <h4>Level 1</h4>
                     <p>Upon entering level one, the user will be presented with an explanation of the sorting algorithm, in this case merge sort. Once they’ve learned about the specific algorithm, they’ll be able to randomly generate numbers to watch an animation of the algorithm working. This animation includes an explanation at each step.</p>
 
                     <h4>Level 2</h4>
-                    <p>Once users have completed level 1, they navigate to level 2 where they will be given more opportunity to test the algorithm themselves. At each step of the algorithm, users will be asked to select the correct answer between two options and drag that answer into the algorithm implementation. Our intentions are to allow the user to check their answer as they go.</p>
+                    <p>Once users have completed level 1, they navigate to level 2 where they will get the opportunity to fill in the blanks. The user will be able to check their answer and recieve extra help. The user only has 3 attempts.</p>
 
                     <h4>Level 3</h4>
-                    <p>After reaching level 3, the difficulty will increase and users will get the opportunity to fill in the blanks. The user will be able to check their answer and receive 3 attempts. A pop up will appear indicating if the response is correct or incorrect.</p>
+                    <p>After reaching level 3, the difficulty will increase as no extra help is given.</p>
+
+                    <h4>Level 4</h4>
+                    <p>Upon reaching level 4, the difficulty increases as 20 numbers (previously 10) are randomly generated out of the range 1-50 (previously 1-20).</p>
+
+                    <h4>Level 5</h4>
+                    <p>Next the difficulty increases as 50 numbers are randomly generated out of the range 1-100.</p>
+
+                    <h4>Custom Level</h4>
+                    <p>The user now has the opportunity to choose the number of numbers and the range to select from.</p>
 
                     <h4>Updates</h4>
                     <p>Upon future updates new features and levels will be integrated into the system. So stay tuned!!!</p>
